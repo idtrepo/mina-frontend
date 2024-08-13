d29edd46a524b9848aae9f3531375ecefa90df97
idtrepo <ifierro@idttecnologias.mx>
2024-08-13 14:47:41 -0700

    feat(src): App modificado

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,51 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <main class="grid grid-cols-12 grid-rows-12 w-full min-h-screen bg-gray-950">
+    <template v-if="autenticado">
+      <section class="p-5 col-start-2 col-end-13 row-start-1 row-end-13 w-full h-full">
+        <div class="grid grid-cols-12 grid-rows-12 w-full h-full">
+          <header class="col-start-1 col-end-13 row-start-1 row-end-3 w-full h-full bg-slate-400">
+            <h2>titulo</h2>
+          </header>
+          <section class="col-start-1 col-end-13 row-start-3 row-end-13 w-full h-full bg-slate-100">
+            <RouterView/>
+          </section>
+        </div>
+      </section>
+      <aside class="col-start-1 col-end-2 row-start-1 row-end-13 w-full h-full bg-slate-50">
+        <p>que tranza</p>
+      </aside>
+    </template>
+    <template v-else>
+      <section class="col-start-1 col-end-13 row-start-1 row-end-13 w-full h-full">
+        <RouterView/>
+      </section>
+    </template>
+  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup>
+import { onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useRouter } from 'vue-router'
+import useAuthStore from './stores/useAuthStore';
+import { VISTA_LOGIN } from './utils/vistas';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+const router = useRouter();
+const authStore = useAuthStore();
+const { autenticado } = storeToRefs(authStore);
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+const verificarSesion = async() => {
+  try{
+    const res = await authStore.verificarSesion();
+    console.log(res);
+  }catch(err){
+    console.error(err);
+    console.log('No hay credenciales');
+    router.push({ name: VISTA_LOGIN });
   }
 }
-</style>
+
+onMounted(() => {
+  verificarSesion();
+})
+</script>
