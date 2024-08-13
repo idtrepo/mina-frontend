6f27925e9bddc2d78ef3058f550c61337da3d4bb
idtrepo <ifierro@idttecnologias.mx>
2024-08-13 14:36:59 -0700

    feat(src/router): Router principal

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import authRouter from './authRouter'
+import modulosRouter from './modulosRouter';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect: { name: 'autenticacion-login' }
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    authRouter,
+    modulosRouter,
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  console.log(to);
+  console.log(from);
+  next();
+});
 
 export default router
