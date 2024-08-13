8dfbef4c8fdbc72dfeef3b1da054f7716c38a63c
idtrepo <ifierro@idttecnologias.mx>
2024-08-13 14:32:35 -0700

    feat(src/services/index): Servicio creador

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { BASE_URL } from "../../config/settings";
+import { interceptorRequest, interceptorRequestError } from "./interceptors/request";
 
 const instancia = axios.create({
     baseURL: BASE_URL
 })
+
+instancia.interceptors.request.use(interceptorRequest, interceptorRequestError);
 
 export default instancia;
