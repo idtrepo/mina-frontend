f9e88f54e1d8bdcc54afcacd56ae8f70967b9f02
idtrepo <ifierro@idttecnologias.mx>
2024-08-15 10:45:20 -0700

    feat(src/services): Nuevos servicios agregados

---
+++ src/services/dataService.js
@@ -0,0 +1,18 @@
+import servicio from './index'
+
+const recurso = 'data/';
+
+export default {
+    obtenerElementos: ({ } = {}) => {
+        return servicio.get(recurso);
+    },
+    obtenerElemento: ({ id }) => {
+        return servicio.get(`${recurso}${id}`);
+    },
+    crearElemento: ({ data }) => {
+        return servicio.post(recurso, data);
+    },
+    editarElemento: ({ id, data }) => {
+        return servicio.patch(`${recurso}${id}`, data);
+    }
+}
