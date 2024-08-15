d05f669aa55d5edc126e451a081b4fa858bfe057
idtrepo <ifierro@idttecnologias.mx>
2024-08-15 10:31:39 -0700

    feat(src/components/base): Componentes de menu agregados

---
+++ src/components/base/BaseMenu.vue
@@ -0,0 +1,33 @@
+<template>
+    <article class="py-6 flex flex-col w-full h-full justify-between">
+        <section class="flex flex-col justify-center items-center">
+            <BaseMenuRuta
+                v-for="ruta in rutas"
+                :ruta="ruta"/>
+        </section>
+        <footer class="flex justify-center items-center">
+            <BaseMenuRuta :ruta="rutaSalida"/>
+        </footer>
+    </article>
+</template>
+
+<script setup>
+import { toRefs } from 'vue'
+import { defineAsyncComponent } from 'vue' 
+
+//Propiedades
+const props = defineProps({
+    rutas: {
+        type: Array,
+        required: true
+    },
+    rutaSalida: {
+        type: Object,
+        required: true
+    }
+})
+const { rutas, rutaSalida } = toRefs(props);
+
+//Componentes
+const BaseMenuRuta = defineAsyncComponent(() => import('@/components/base/BaseMenuRuta.vue'))
+</script>
