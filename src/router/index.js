a6ca09592754dc7f8ee5c19e26f92121d31ddf89
idtrepo <ifierro@idttecnologias.mx>
2024-08-15 10:29:40 -0700

    feat(src/router): Guard agregado

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
+import { storeToRefs } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router'
 import authRouter from './authRouter'
 import modulosRouter from './modulosRouter';
+import { VISTA_LOGIN } from '@/utils/vistas';
+import useAuthStore from '@/stores/useAuthStore';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,9 +18,16 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  console.log(to);
-  console.log(from);
-  next();
+  const authStore = useAuthStore();
+  const { autenticado } = storeToRefs(authStore);
+
+  if (!autenticado.value && to.name !== VISTA_LOGIN) {
+    console.log('NO ESTAS AUTENTICADO, NO PUEDES PASAR');
+    next({ name: VISTA_LOGIN });
+  } else {
+    console.log('ESTAS AUTENTICADO, PUEDES PASAR');
+    next();
+  }
 });
 
 export default router
