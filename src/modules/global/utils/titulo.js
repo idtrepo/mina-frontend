4cedf7d9235a877ab28fedf7ad5dfc76ed747e06
idtrepo <ifierro@idttecnologias.mx>
2024-08-21 11:37:45 -0700

    feat(modules/global/utils): Diccionario de titulos agregados

---
+++ src/modules/global/utils/titulo.js
@@ -0,0 +1,8 @@
+export const TITULO = {
+    AREAS: 'areas',
+    CLIENTES: 'clientes',
+    MODULOS: 'modulos',
+    PERFILES: 'perfiles',
+    SUCURSALES: 'sucursales',
+    USUARIOS: 'usuarios'
+}
