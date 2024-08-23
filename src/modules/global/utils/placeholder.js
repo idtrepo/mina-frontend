7c5d474ef17645ab20706594a3ce915abecffdea
idtrepo <ifierro@idttecnologias.mx>
2024-08-23 10:46:56 -0700

    feat(modules/global/utils/placeholder): Placeholde de modulos agregado

--- src/modules/global/utils/placeholder.js
+++ src/modules/global/utils/placeholder.js
@@ -12,3 +12,10 @@ export const USUARIOS_PLACEHOLDER = {
     PERFIL: 'Seleccione el perfil del usuario',
     CLIENTE: 'Seleccione el cliente asociado al usuario'
 }
+
+export const MODULOS_PLACEHOLDER = {
+    MAC: 'Introduce la MAC del modulo',
+    CLIENTE: 'Seleccione el cliente al que desea asociar el modulo',
+    SUCURSAL: 'Seleccione la sucursal a la que desea asocial el modulo',
+    AREA: 'Seleccione el area a la que desea asociar el modulo'
+}
