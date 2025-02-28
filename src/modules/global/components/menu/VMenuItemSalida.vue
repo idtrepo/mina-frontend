ab0948e04e9141ccaa42b0e87d7f7dcf590a07c5
Luis Mario Torres Moreno <luisillo77mario@gmail.com>
2025-02-27 17:08:16 -0700

    refactor(global/components/menu): cambio en colores y menu lateral reactivo al perfil del usuario

--- src/modules/global/components/menu/VMenuItemSalida.vue
+++ src/modules/global/components/menu/VMenuItemSalida.vue
@@ -1,5 +1,5 @@
 <template>
-    <article @click="cerrarSesion" class="inline py-6 lg:py-0 flex-grow text-center transition-all duration-200 hover:bg-cyan-700/50 rounded-tr-3xl lg:rounded-tr-none hover:cursor-pointer ">
+    <article @click="cerrarSesion" class="inline py-6 px-6 text-center transition-all duration-200 hover:bg-gray-700/50 rounded-3xl  hover:cursor-pointer ">
         <i class="" :class="ICONOS.SALIDA"></i>
         <span class="hidden lg:inline">salir</span>
     </article>
