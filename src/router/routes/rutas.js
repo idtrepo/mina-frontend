30dcfea1b08bcfa0e001344a28e5a48355e1ee95
Luis Mario Torres Moreno <luisillo77mario@gmail.com>
2025-02-27 17:10:20 -0700

    fix(router/routes/rutas): arreglado el nombre de la propiedad SUPER_USUARIO

--- src/router/routes/rutas.js
+++ src/router/routes/rutas.js
@@ -4,37 +4,37 @@ import { PERFILES } from "@/modules/global/utils/perfiles";
 
 export const rutas = [
     {
-        perfil:[PERFILES.SUPERUSUARIO, PERFILES.ADMINISTRADOR, PERFILES.SUPERVISOR],
+        perfil:[PERFILES.SUPER_USUARIO, PERFILES.ADMINISTRADOR, PERFILES.SUPERVISOR],
         titulo: 'areas',
         icono: ICONOS.AREAS,
         ruta: VISTAS.AREAS
     },
     {
-        perfil:[PERFILES.SUPERUSUARIO],
+        perfil:[PERFILES.SUPER_USUARIO],
         titulo: 'clientes',
         icono: ICONOS.CLIENTES,
         ruta: VISTAS.CLIENTES
     },
     {
-        perfil:[PERFILES.SUPERUSUARIO, PERFILES.ADMINISTRADOR, PERFILES.SUPERVISOR, PERFILES.OPERADOR],
+        perfil:[PERFILES.SUPER_USUARIO, PERFILES.ADMINISTRADOR, PERFILES.SUPERVISOR, PERFILES.OPERADOR],
         titulo: 'modulos',
         icono: ICONOS.MODULOS,
         ruta: VISTAS.MODULOS
     },
     {
-        perfil:[PERFILES.SUPERUSUARIO, PERFILES.ADMINISTRADOR],
+        perfil:[PERFILES.SUPER_USUARIO, PERFILES.ADMINISTRADOR],
         titulo: 'perfiles',
         icono: ICONOS.PERFILES,
         ruta: VISTAS.PERFILES
     },
     {
-        perfil:[PERFILES.SUPERUSUARIO, PERFILES.ADMINISTRADOR],
+        perfil:[PERFILES.SUPER_USUARIO, PERFILES.ADMINISTRADOR, PERFILES.SUPERVISOR],
         titulo: 'sucursales',
         icono: ICONOS.SUCURSALES,
         ruta: VISTAS.SUCURSALES
     },
     {
-        perfil:[PERFILES.SUPERUSUARIO],
+        perfil:[PERFILES.SUPER_USUARIO, PERFILES.ADMINISTRADOR, PERFILES.SUPERVISOR],
         titulo: 'usuarios',
         icono: ICONOS.USUARIOS,
         ruta: VISTAS.USUARIOS
