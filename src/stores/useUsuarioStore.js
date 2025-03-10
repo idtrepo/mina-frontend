0cb3125460983abcde7db99412519206ebd3e01f
Luis Mario Torres Moreno <luisillo77mario@gmail.com>
2025-03-10 11:12:45 -0700

    feat(src/views): agregadas vistas para los detalles y los datos de los modulos

---
+++ src/stores/useUsuarioStore.js
@@ -0,0 +1,35 @@
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+
+export default defineStore("usuario-store", () => {
+  const usuario = ref(null);
+  const usuarioNombre = computed(() => usuario.value?.nombre ?? "");
+  const usuarioApellido = computed(() => usuario.value?.apellido ?? "");
+  const usuarioNombreCompleto = computed(
+    () => `${usuarioNombre.value} ${usuarioApellido.value}`
+  );
+  const usuarioCorreo = computed(() => usuario.value?.correo);
+  const usuarioPerfil = computed(() => usuario.value?.perfil?.nombre);
+  const usuarioPermisos = computed(() => usuario.value?.perfil?.acciones);
+  const usuarioCliente = computed(() => usuario.value?.cliente?.id);
+  const usuarioSucursal = computed(() => usuario.value?.sucursal?.id);
+
+  const asignarDataUsuario = (dataUsuario) => {
+    usuario.value = dataUsuario;
+  };
+
+  const borrarDataUsuario = () => (usuario.value = null);
+
+  return {
+    usuarioNombre,
+    usuarioApellido,
+    usuarioNombreCompleto,
+    usuarioCorreo,
+    usuarioPerfil,
+    usuarioPermisos,
+    usuarioCliente,
+    usuarioSucursal,
+    asignarDataUsuario,
+    borrarDataUsuario,
+  };
+});
