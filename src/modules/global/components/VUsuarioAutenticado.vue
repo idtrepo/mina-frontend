5dcc14f8a60694077196997b0ef4c609b9bdaff0
idtrepo <ifierro@idttecnologias.mx>
2025-03-12 11:01:48 -0700

    feat(global/components/VUsuarioAutenticado): boton cerrar sesion agregado

--- src/modules/global/components/VUsuarioAutenticado.vue
+++ src/modules/global/components/VUsuarioAutenticado.vue
@@ -1,19 +1,25 @@
 <template>
-    <article class="flex items-center transition-all duration-200 text-xs border-l-4 border-l-amber-600/0 hover:cursor-pointer">
-        <header class="bg-slate-900 mr-2 w-7 h-7 flex justify-center items-center rounded-full">
-            <i :class="ICONOS.USUARIOS" class=""></i>
-        </header>
-        <section>
-            <h3 class="uppercase font-bold">{{ usuarioNombreCompleto }}</h3>
-            <p class="uppercase">{{ usuarioPerfil }}</p>
-        </section>
-    </article>
+    <NPopover trigger="click">
+        <template #trigger>
+            <article class="flex items-center transition-all duration-200 text-xs border-l-4 border-l-amber-600/0 hover:cursor-pointer">
+                <header class="bg-slate-900 mr-2 w-7 h-7 flex justify-center items-center rounded-full">
+                    <i :class="ICONOS.USUARIOS" class=""></i>
+                </header>
+                <section>
+                    <h3 class="uppercase font-bold">{{ usuarioNombreCompleto }}</h3>
+                    <p class="uppercase">{{ usuarioPerfil }}</p>
+                </section>
+            </article>
+        </template>
+        <NButton type="error" class="w-full">cerrar sesion</NButton>
+    </NPopover>
 </template>
 
 <script setup>
 import { storeToRefs } from 'pinia';
 import useUsuarioStore from '@/modules/auth/stores/useUsuarioStore';
 import { ICONOS } from '../utils/iconos';
+import { NPopover, NButton } from 'naive-ui';
 
 // dependencias
 const usuarioStore = useUsuarioStore();
