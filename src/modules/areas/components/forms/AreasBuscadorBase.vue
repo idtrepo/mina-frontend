ec8b7104c59847e18f9796d990e486978616069a
idtrepo <ifierro@idttecnologias.mx>
2025-03-13 10:38:14 -0700

    feat(areas/components/forms): formularios agregados

---
+++ src/modules/areas/components/forms/AreasBuscadorBase.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">nombre</p>
+            <NInput v-model:value="filtros.nombre"/>
+        </article>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">sucursal</p>
+            <NSelect
+                :options="sucursalesOpciones"
+                v-model:value="filtros.sucursal"/>
+        </article>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">fecha</p>
+            <NDatePicker
+                v-model:value="filtros.fecha"
+                clearable/>
+        </article>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">estatus</p>
+            <NSwitch
+                v-model:value="filtros.estatus"/>
+        </article>
+    </section>
+</template>
+
+<script setup>
+import { NDatePicker, NSwitch, NSelect, NInput } from 'naive-ui'
+import useAreas from '../../composables/useAreas';
+import useSucursales from '@/modules/sucursales/composables/useSucursales';
+
+//dependencias
+const { filtros } = useAreas();
+const { sucursalesOpciones } = useSucursales();
+</script>
---
+++ src/modules/areas/components/forms/AreasFormularioBase.vue
@@ -0,0 +1,29 @@
+<template>
+    <section>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">nombre</p>
+            <NInput 
+                v-model:value="area.nombre"
+                clearable
+                :disabled="edicionHabilitada"/>
+        </article>
+        <article class="mb-1 uppercase">
+            <p>sucursal</p>
+            <NSelect 
+                v-model:value="area.idSucursal"
+                clearable
+                :options="sucursalesOpciones"
+                :disabled="edicionHabilitada"/>
+        </article>
+    </section>
+</template>
+
+<script setup>
+import { NInput, NSelect } from 'naive-ui'
+import useAreas from '../../composables/useAreas';
+import useSucursales from '@/modules/sucursales/composables/useSucursales';
+
+//dependencias
+const { area, edicionHabilitada } = useAreas();
+const { sucursalesOpciones } = useSucursales();
+</script>
