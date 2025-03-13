daf51675e6ceb747ec33b9e8a73e6e5661c74db0
idtrepo <ifierro@idttecnologias.mx>
2025-03-13 12:17:56 -0700

    feat(clientes/components/forms): formularios agregados

---
+++ src/modules/clientes/components/forms/ClientesFormularioBase.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">nombre</p>
+            <NInput 
+                v-model:value="cliente.nombre"
+                clearable
+                :disabled="edicionHabilitada"/>
+        </article>
+    </section>
+</template>
+
+<script setup>
+import { NInput, NSelect } from 'naive-ui'
+import useClientes from '../../composables/useClientes';
+
+//dependencias
+const { cliente, edicionHabilitada } = useClientes();
+</script>
