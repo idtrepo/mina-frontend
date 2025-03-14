b9baeef091f56c3346dcf92e44d7989b4c99d789
idtrepo <ifierro@idttecnologias.mx>
2025-03-14 14:37:20 -0700

    feat(modulos/composables/useModulos): logica modificada

--- src/modules/modulos/composables/useModulos.js
+++ src/modules/modulos/composables/useModulos.js
@@ -1,146 +1,144 @@
-import {computed, toValue} from 'vue';
-import {storeToRefs} from 'pinia';
-import useModulosStore from '../stores/useModulosStore';
-import {useRouter} from 'vue-router';
-import {ModulosService} from '@/modules/modulos/services/modulosService';
-import {ICONOS} from '@/modules/global/utils/iconos';
-import {evaluarModulo} from '../schemas/modulosSchema';
-import useRequest from '@/composables/request/useRequest';
-import useFiltrosStore from '@/stores/useFiltrosStore';
-import useEdicionStore from '@/stores/useEdicionStore';
-import {VISTAS} from '@/modules/global/utils/vistas';
-import {reiniciarData} from '@/utils/reinicio';
-import useDataStore from '../stores/useDataStore';   
+import { computed, toValue } from "vue";
+import { storeToRefs } from "pinia";
+import useModulosStore from "../stores/useModulosStore";
+import { useRouter } from "vue-router";
+import { ModulosService } from "@/modules/modulos/services/modulosService";
+import { ICONOS } from "@/modules/global/utils/iconos";
+import { evaluarModulo, evaluarModuloParcial } from "../schemas/modulosSchema";
+import useRequest from "@/composables/request/useRequest";
+import useFiltrosStore from "@/stores/useFiltrosStore";
+import useEdicionStore from "@/stores/useEdicionStore";
+import { VISTAS } from "@/modules/global/utils/vistas";
+import { reiniciarData } from "@/utils/reinicio";
+import useDataStore from "../stores/useDataStore";
 
 export default () => {
-    const router = useRouter();
-    const filtrosStore = useFiltrosStore();
-    const edicionStore = useEdicionStore();
-    const moduloStore = useModulosStore();
-    const {editar, edicionHabilitada} = storeToRefs(edicionStore);
-    const {filtros, filtrosMapeados} = storeToRefs(filtrosStore);
-    const {modulo, modulos, numeroElementos, modulosOpciones} = storeToRefs(moduloStore);
-    const {datos, asignarData} = useDataStore();
+  const router = useRouter();
+  const filtrosStore = useFiltrosStore();
+  const edicionStore = useEdicionStore();
+  const moduloStore = useModulosStore();
+  const { editar, edicionHabilitada } = storeToRefs(edicionStore);
+  const { filtros, filtrosMapeados } = storeToRefs(filtrosStore);
+  const { modulo, modulos, numeroElementos, modulosOpciones } =
+    storeToRefs(moduloStore);
+  const { datos, asignarData } = useDataStore();
 
-    const {obtenerElemento, obtenerElementos, crearElemento, editarElemento, obtenerElementoData} = useRequest({
-        servicio: ModulosService,
-        filtros: filtrosMapeados,
-        evaluacion: evaluarModulo
-    });
+  const {
+    obtenerElemento,
+    obtenerElementos,
+    crearElemento,
+    editarElemento,
+    obtenerElementoData,
+  } = useRequest({
+    servicio: ModulosService,
+    filtros: filtrosMapeados,
+    evaluacion: evaluarModulo,
+    evaluacionParcial: evaluarModuloParcial,
+  });
 
-    //listado de modulos
-    const modulosListado = computed(() => modulos.value.map(({id, mac, sucursal, area}) => ({
-        id,
-        titulo: mac,
-        subtitulo: sucursal.nombre+': '+area.nombre,
-        icono: ICONOS.MODULOS,
-        accion: () => router.push({name: VISTAS.MODULOS_DATA, params: { id }})
-    })));
+  //listado de modulos
+  const modulosListado = computed(() =>
+    modulos.value.map(({ id, mac, sucursal, area }) => ({
+      id,
+      titulo: mac,
+      subtitulo: sucursal.nombre + ": " + area.nombre,
+      icono: ICONOS.MODULOS,
+      accion: () => router.push({ name: VISTAS.MODULOS_DATA, params: { id } }),
+    }))
+  );
 
-    const obtenerModulos = async ({params = null}) => {
-        try {
-            const res = await obtenerElementos({params});
-            if (res) {
-                moduloStore.asignarDataModulos(res.data);
-            }
+  const obtenerModulos = async ({ params = null } = {}) => {
+    const res = await obtenerElementos({ params });
 
-            return res;
-        } catch (err) {
-            console.log(err)
-            throw err;
-        }
-    };
-
-    const obtenerModulo = async ({id}) => {
-        try{
-            const res = await obtenerElemento({id});
-            if(res){
-                moduloStore.asignarDataModulo(res.data.data);
-            }
-
-            return res;
-        } catch (err) {
-            console.log(err)
-            throw err;
-        }
-    };
-
-    const obtenerDataModulo = async ({id}) => {
-        try {
-            const res = await obtenerElementoData({id});
-            if (res) {
-                asignarData(res.data);
-            }
-
-            return res;
-        } catch (err) {
-            console.log(err)
-            throw err;
-        }
+    if (res) {
+      moduloStore.asignarDataModulos(res);
     }
 
-    const crearModulo = async ({data}) => {
-        try {
-            const res = await crearElemento(data);
-            if (res) {
-                moduloStore.asignarDataModulo(res);
-            }
+    return res;
+  };
 
-            return res;
-        } catch (err) {
-            console.log(err)
-            throw err;
-        }
-    };
+  const obtenerModulo = async ({ id }) => {
+    const res = await obtenerElemento({ id });
 
-    const editarModulo = async () => {
-        try {
-            const res = await editarElemento({dataElemento: modulo.value});
-            if (res) {
-                moduloStore.asignarDataModulo(res.data);
-            }
-
-            return res;
-        } catch (err) {
-            console.log(err)
-            throw err;
-        }
-    };
-
-    const habilitarEdicion = () => {
-        edicionStore.habilitarEdicion();
-
-        if(editar.value){
-            edicionStore.guardarData(modulo.data);
-        } else {
-            modulo.value = edicionStore.borrarData(); 
-        }
+    if (res) {
+      moduloStore.asignarDataModulo(res);
     }
 
-    function reiniciarDataCreacion() {
-        modulo.value = reiniciarData(toValue(modulo));
+    return res;
+  };
+
+  const obtenerDataModulo = async ({ id }) => {
+    try {
+      const res = await obtenerElementoData({ id });
+      if (res) {
+        asignarData(res.data);
+      }
+
+      return res;
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  const crearModulo = async ({ data }) => {
+    const res = await crearElemento({ dataElemento: modulo });
+
+    if (res) {
+      reiniciarDataCreacion();
+      await obtenerModulos();
     }
 
-    function reiniciarDataModulos() {
-        reiniciarDataCreacion();
-        filtrosStore.reiniciarFiltros();
-        edicionStore.reiniciarEdicion();
-        modulos.value = [];
+    return res;
+  };
+
+  const editarModulo = async () => {
+    const res = await editarElemento({ dataElemento: modulo });
+
+    if (res) {
+      editar.value = false;
+      const { data: modulo } = res;
+      await obtenerCliente(modulo);
     }
 
-    return {
-        modulo,
-        modulos,
-        numeroElementos,
-        modulosOpciones,
-        modulosListado,
-        edicionHabilitada,
-        obtenerModulos,
-        obtenerModulo,
-        crearModulo,
-        editarModulo,
-        habilitarEdicion,
-        reiniciarDataModulos,
-        obtenerDataModulo
+    return res;
+  };
+
+  const habilitarEdicion = () => {
+    edicionStore.habilitarEdicion();
+
+    if (editar.value) {
+      edicionStore.guardarData(modulo);
+    } else {
+      modulo.value = edicionStore.borrarData();
     }
-}
+  };
+
+  function reiniciarDataCreacion() {
+    modulo.value = reiniciarData(toValue(modulo));
+  }
+
+  function reiniciarDataModulos() {
+    reiniciarDataCreacion();
+    filtrosStore.reiniciarFiltros();
+    edicionStore.reiniciarEdicion();
+    modulos.value = [];
+  }
+
+  return {
+    modulo,
+    modulos,
+    filtros,
+    numeroElementos,
+    modulosOpciones,
+    modulosListado,
+    edicionHabilitada,
+    obtenerModulos,
+    obtenerModulo,
+    crearModulo,
+    editarModulo,
+    habilitarEdicion,
+    reiniciarDataModulos,
+    reiniciarDataCreacion,
+    obtenerDataModulo,
+  };
+};
