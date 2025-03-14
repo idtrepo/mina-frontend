132cbf5afd5c9494c6ac107a254d06bf4df05c62
idtrepo <ifierro@idttecnologias.mx>
2025-03-14 14:39:17 -0700

    refactor(views/VMainView): estilos css modificados

--- src/views/VMainView.vue
+++ src/views/VMainView.vue
@@ -1,22 +1,25 @@
 <template>
-    <main class="w-screen h-dvh grid grid-cols-12 grid-rows-12 text-slate-300 bg-slate-950">
-        <template v-if="autenticado">
-            <header class="px-5 flex justify-between items-center col-span-full lg:col-start-2 lg:col-end-13 row-span-1 bg-slate-900/50">
-                <img :src="imgPromev" alt="promev" class="w-36 -ml-4">
-                <VUsuarioAutenticado/>
-            </header>
-            <section class="px-5 pt-8 col-span-full lg:col-start-2 lg:col-end-13 row-span-10 lg:row-span-11">
-                <RouterView/>
-            </section>
-            <aside class="col-span-full lg:col-start-1 lg:col-end-2 row-span-1 lg:row-span-full">
-                <VMenuList/>
-            </aside>
-        </template>
-        <template v-else>
-            <section class="col-span-full row-span-full">
-                <RouterView/>
-            </section>
-        </template>
+    <main class="w-screen h-dvh text-slate-300 bg-slate-950">
+        <div class="w-full h-full grid grid-cols-12 grid-rows-12">
+            <template v-if="autenticado">
+                <header
+                    class="px-5 flex justify-between items-center col-span-full lg:col-start-2 lg:col-end-13 row-span-1 bg-slate-900/50">
+                    <img :src="imgPromev" alt="promev" class="w-36 -ml-4">
+                    <VUsuarioAutenticado />
+                </header>
+                <section class="px-5 pt-8 col-span-full lg:col-start-2 lg:col-end-13 row-span-10 lg:row-span-11">
+                    <RouterView />
+                </section>
+                <aside class="col-span-full lg:col-start-1 lg:col-end-2 row-span-1 lg:row-span-full">
+                    <VMenuList />
+                </aside>
+            </template>
+            <template v-else>
+                <section class="col-span-full row-span-full">
+                    <RouterView />
+                </section>
+            </template>
+        </div>
     </main>
 </template>
 
