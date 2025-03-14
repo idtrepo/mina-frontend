38e26285d50a0d5eec7954c88a7e41e1ae3e12fc
idtrepo <ifierro@idttecnologias.mx>
2025-03-14 14:36:45 -0700

    refactor(modulos/stores/useModulostore): sintaxis modificada

--- src/modules/modulos/stores/useModulosStore.js
+++ src/modules/modulos/stores/useModulosStore.js
@@ -1,43 +1,44 @@
-import {ref,  computed} from 'vue';
+import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 
-export default defineStore('modulos-store', () => {
-    const modulo = ref({
-        mac: null,
-        idArea: null,
-        idSucursal: null, 
-        idCliente: null
-    });
+export default defineStore("modulos-store", () => {
+  const modulo = ref({
+    mac: null,
+    idArea: null,
+    idSucursal: null,
+    idCliente: null,
+  });
 
-    const modulos = ref([]);
-    const numeroElementos = ref(1);
+  const modulos = ref([]);
+  const numeroElementos = ref(1);
 
-    const asignarDataModulos = ({data, resultados}) => {
-        console.log(data)
-        modulos.value = data;
-        numeroElementos.value = resultados;
-    };
+  const asignarDataModulos = ({ data, resultados }) => {
+    modulos.value = data;
+    numeroElementos.value = resultados;
+  };
 
-    const asignarDataModulo = (data) => {
-        const {id, mac, area, sucursal, cliente} = data;
-        modulo.value["id"] = id;
-        modulo.value.mac = mac;
-        modulo.value.idArea = area.id;
-        modulo.value.idSucursal = sucursal.id;
-        modulo.value.idCliente = cliente.id;
-}
+  const asignarDataModulo = ({ data }) => {
+    const { id, mac, area, sucursal, cliente } = data;
+    modulo.value["id"] = id;
+    modulo.value.mac = mac;
+    modulo.value.idArea = area.id;
+    modulo.value.idSucursal = sucursal.id;
+    modulo.value.idCliente = cliente.id;
+  };
 
-const modulosOpciones = computed(() => modulos.value.map(({id, mac}) => ({
-    label: mac,
-    value: id,
-})));
+  const modulosOpciones = computed(() =>
+    modulos.value.map(({ id, mac }) => ({
+      label: mac,
+      value: id,
+    }))
+  );
 
-return {
+  return {
     modulo,
     modulos,
     numeroElementos,
     asignarDataModulos,
     asignarDataModulo,
-    modulosOpciones
-}
-})
+    modulosOpciones,
+  };
+});
