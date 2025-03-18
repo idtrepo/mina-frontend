bf9544ab7654e5e27d87d71cb26930b0c1e6d8b6
idtrepo <ifierro@idttecnologias.mx>
2025-03-18 12:33:17 -0700

    refactor(sucursales/stores/useSucursalesStore): codigo formateado

--- src/modules/sucursales/stores/useSucursalesStore.js
+++ src/modules/sucursales/stores/useSucursalesStore.js
@@ -1,39 +1,41 @@
-import {ref, computed} from 'vue';
+import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 
-export default defineStore('sucursales-store', () => {
-    const sucursal = ref({
-        nombre: null,
-        idCliente: null
-    });
+export default defineStore("sucursales-store", () => {
+  const sucursal = ref({
+    nombre: null,
+    idCliente: null,
+  });
 
-    const sucursales = ref([]);
-    const numeroElementos = ref(1);
+  const sucursales = ref([]);
+  const numeroElementos = ref(1);
 
-    const asignarDataSucursales = ({data, resultados}) => {
-        sucursales.value = data;
-        numeroElementos.value = resultados;
-    };
+  const asignarDataSucursales = ({ data, resultados }) => {
+    sucursales.value = data;
+    numeroElementos.value = resultados;
+  };
 
-    const asignarDataSucursal = (data) => {
-        const {id, nombre, cliente} = data;
-        console.log(data)
-        sucursal.value["id"] = id;
-        sucursal.value.nombre = nombre;
-        sucursal.value.idCliente = cliente.id;
-    }
+  const asignarDataSucursal = ({ data }) => {
+    const { id, nombre, cliente } = data;
 
-    const sucursalesOpciones = computed(() => sucursales.value.map(({id, nombre}) => ({
-        label: nombre,
-        value: id,
-    })));
+    sucursal.value["id"] = id;
+    sucursal.value.nombre = nombre;
+    sucursal.value.idCliente = cliente.id;
+  };
 
-    return {
-        sucursal,
-        sucursales,
-        numeroElementos,
-        asignarDataSucursales,
-        asignarDataSucursal,
-        sucursalesOpciones
-    }
-})
+  const sucursalesOpciones = computed(() =>
+    sucursales.value.map(({ id, nombre }) => ({
+      label: nombre,
+      value: id,
+    }))
+  );
+
+  return {
+    sucursal,
+    sucursales,
+    numeroElementos,
+    asignarDataSucursales,
+    asignarDataSucursal,
+    sucursalesOpciones,
+  };
+});
