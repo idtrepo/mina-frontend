be7c50c997f71bafc6ea79f4d7a653a90b371525
idtrepo <ifierro@idttecnologias.mx>
2025-03-18 10:36:21 -0700

    feat(perfiles/components/forms): formularios agregados

---
+++ src/modules/perfiles/components/forms/PerfilesBuscadorBase.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">nombre</p>
+            <NInput v-model:value="filtros.nombre"/>
+        </article>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">fecha</p>
+            <NDatePicker
+                v-model:value="filtros.fecha"
+                clearable/>
+        </article>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">estatus</p>
+            <NSwitch
+                v-model:value="filtros.estatus"/>
+        </article>
+    </section>
+</template>
+
+<script setup>
+import { NDatePicker, NSwitch, NSelect, NInput } from 'naive-ui'
+import usePerfiles from '../../composables/usePerfiles';
+
+// dependencias
+const { filtros } = usePerfiles();
+</script>
---
+++ src/modules/perfiles/components/forms/PerfilesFormulario.vue
@@ -0,0 +1,25 @@
+<template>
+    <VFormularioBase :="confFormulario">
+        <template #contenido>
+            <PerfilesFormularioBase/>
+        </template>
+    </VFormularioBase>
+</template>
+
+<script setup>
+import { ref, defineAsyncComponent } from 'vue';
+import usePerfiles from '../../composables/usePerfiles';
+
+// dependencias
+const { crearPerfil } = usePerfiles();
+
+// componentes
+const VFormularioBase = defineAsyncComponent(() => import('@/components/forms/VFormularioBase.vue'));
+const PerfilesFormularioBase = defineAsyncComponent(() => import('./PerfilesFormularioBase.vue'));
+
+// configuracion formulario
+const confFormulario = ref({
+    tituloBoton: 'crear',
+    accionFormulario: crearPerfil,
+})
+</script>
