1d4df34bee0365ab69e0be49d87a2dd4590b3183
idtrepo <ifierro@idttecnologias.mx>
2025-03-18 10:40:44 -0700

    refactor(perfiles/views/PerfilesDataView): Vista modificada

--- src/modules/perfiles/views/PerfilesDataView.vue
+++ src/modules/perfiles/views/PerfilesDataView.vue
@@ -1,32 +1,30 @@
 <template>
-    <VDataView
+    <VDataView 
         :peticiones="peticiones"
         :reiniciar-data="reiniciarDataPerfiles"
         :habilitar-edicion="habilitarEdicion"
         :editar-elemento="editarPerfil">
         <template #formulario>
-            <PerfilesFormBase />
-        </template>    
+            <PerfilesFormularioBase/>
+        </template>
     </VDataView>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { defineAsyncComponent } from 'vue'
-import { useRoute } from 'vue-router'
-import usePerfiles from '../composables/usePerfiles'
+import { useRoute } from 'vue-router';
+import { defineAsyncComponent, ref } from 'vue';
+import usePerfiles from '../composables/usePerfiles';
 
 // dependencias
 const route = useRoute();
-const {obtenerPerfil, editarPerfil, reiniciarDataPerfiles, habilitarEdicion} = usePerfiles();
-
-const peticiones = ref([
-    obtenerPerfil({id: route.params.id}),
-])
+const { obtenerPerfil, reiniciarDataPerfiles, habilitarEdicion, editarPerfil } = usePerfiles();
 
 // componentes
 const VDataView = defineAsyncComponent(() => import('@/views/detalles/VDataView.vue'));
-const PerfilesFormBase = defineAsyncComponent(() => import('../components/forms/PerfilesFormBase.vue'));
+const PerfilesFormularioBase = defineAsyncComponent(() => import('../components/forms/PerfilesFormularioBase.vue'));
 
-
+// configuracion vista
+const peticiones = ref([
+    obtenerPerfil({ id: route.params.id }),
+])
 </script>
