bfc199e06dd91b0b497f6dfe31b1ded5b40123d7
idtrepo <ifierro@idttecnologias.mx>
2025-03-19 14:52:41 -0700

    refactor(usuarios/views/UsuariosView): maquedato modificado

--- src/modules/usuarios/views/UsuariosView.vue
+++ src/modules/usuarios/views/UsuariosView.vue
@@ -1,48 +1,49 @@
 <template>
-    <VListadoView
-        :elementos="usuariosListado"
-        :numElementos="numeroElementos"
-        :obtenerListado = "obtenerUsuarios"
-        :reiniciarData = "reiniciarDataCreacion">
-        <template #buscador>
-            <UsuariosBuscador/>
+    <VListadoView :elementos="usuariosListado" :numElementos="numeroElementos" :obtenerListado="obtenerUsuarios"
+        :reiniciarData="reiniciarDataCreacion">
+        <template #formulario-buscar>
+            <UsuariosBuscador />
         </template>
-        <template #formulario>
-            <UsuariosForm/>
+        <template #formulario-agregar>
+            <UsuariosFormulario />
         </template>
     </VListadoView>
 </template>
 
 <script setup>
-import { onMounted, provide, onUnmounted } from 'vue'
+import { onMounted, onUnmounted } from 'vue'
 import { defineAsyncComponent } from 'vue'
 import useUsuarios from '../composables/useUsuarios';
+import usePerfiles from '@/modules/perfiles/composables/usePerfiles';
 import useClientes from '@/modules/clientes/composables/useClientes';
 import useSucursales from '@/modules/sucursales/composables/useSucursales';
 import useAreas from '@/modules/areas/composables/useAreas';
 
 // dependencias
-const {obtenerClientes} = useClientes();
-const {obtenerSucursales} = useSucursales();
-const {obtenerAreas} = useAreas();
-const {numeroElementos, usuariosListado, obtenerUsuarios, reiniciarDataUsuarios, reiniciarDataCreacion} = useUsuarios();
+const { usuariosListado, numeroElementos, obtenerUsuarios, reiniciarDataCreacion, reiniciarDataUsuarios } = useUsuarios();
+const { obtenerPerfiles } = usePerfiles();
+const { obtenerClientes } = useClientes();
+const { obtenerSucursales } = useSucursales();
+const { obtenerAreas } = useAreas();
+
 // componentes
 const VListadoView = defineAsyncComponent(() => import('@/views/listado/VListadoView.vue'));
-const UsuariosForm = defineAsyncComponent(() => import('@/modules/usuarios/components/forms/UsuariosForm.vue'));
-const UsuariosBuscador = defineAsyncComponent(() => import('@/modules/usuarios/components/forms/UsuariosBuscador.vue'));
+const UsuariosBuscador = defineAsyncComponent(() => import('../components/forms/UsuariosBuscador.vue'));
+const UsuariosFormulario = defineAsyncComponent(() => import('../components/forms/UsuariosFormulario.vue'));
+
 
 onMounted(() => {
     Promise.allSettled([
-        obtenerClientes({params:{ listado: true }}),
-        obtenerSucursales({params:{ listado: true }}),
-        obtenerAreas({params:{ listado: true }}),
-        obtenerUsuarios({params:{ listado: true }})
+        obtenerPerfiles({ params: { listado: true } }),
+        obtenerClientes({ params: { listado: true } }),
+        obtenerSucursales({ params: { listado: true } }),
+        obtenerAreas({ params: { listado: true } }),
     ])
         .then(console.log)
         .catch(console.log);
 });
 
 onUnmounted(() => {
-    reiniciarDataUsuarios();
+    reiniciarDataUsuarios()
 })
 </script>
