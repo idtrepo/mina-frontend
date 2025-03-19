82755971dfa40ce0767a1db36e66a8dbad4cc217
idtrepo <ifierro@idttecnologias.mx>
2025-03-19 14:47:23 -0700

    feat(utils/perfiles): constantes agregadas

--- src/utils/perfiles.js
+++ src/utils/perfiles.js
@@ -2,5 +2,12 @@ export const PERFILES = {
   SUPERUSUARIO: "superusuario",
   ADMINISTRADOR: "administrador",
   SUPERVISOR: "supervisor",
-  OPERADOR:"operador"
+  OPERADOR: "operador",
+};
+
+export const PERFILES_ID = {
+  SUPERUSUARIO: 1,
+  ADMINISTRADOR: 2,
+  SUPERVISOR: 3,
+  OPERADOR: 4,
 };
