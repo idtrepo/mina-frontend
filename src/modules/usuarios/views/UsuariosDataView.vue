9477aa097ef08d48e270d46c19b8a7d6fc541c31
idtrepo <ifierro@idttecnologias.mx>
2025-03-19 14:51:44 -0700

    refactor(usuarios/views/UsuariosDataViews): maquetado modificado

--- src/modules/usuarios/views/UsuariosDataView.vue
+++ src/modules/usuarios/views/UsuariosDataView.vue
@@ -1,43 +1,42 @@
 <template>
-    <VDataView
+    <VDataView 
         :peticiones="peticiones"
         :reiniciar-data="reiniciarDataUsuarios"
         :habilitar-edicion="habilitarEdicion"
-        :editar-elemento="editarUsuario">>               
-            <template #formulario>
-                <UsuarioFormBase/>
-            </template>      
+        :editar-elemento="editarUsuario">
+        <template #formulario>
+            <UsuariosFormularioBase/>
+        </template>
     </VDataView>
 </template>
 
 <script setup>
-import { defineAsyncComponent } from 'vue'
-import { ref } from 'vue'
-import { useRoute } from 'vue-router'
-import useUsuarios from '../composables/useUsuarios'
-import usePerfiles from '@/modules/perfiles/composables/usePerfiles'
-import useClientes from '@/modules/clientes/composables/useClientes'
-import useAreas from '@/modules/areas/composables/useAreas'
+import { useRoute } from 'vue-router';
+import { defineAsyncComponent, ref } from 'vue';
+import useUsuarios from '../composables/useUsuarios';
+import useAreas from '@/modules/areas/composables/useAreas';
+import useClientes from '@/modules/clientes/composables/useClientes';
+import usePerfiles from '@/modules/perfiles/composables/usePerfiles';
 import useSucursales from '@/modules/sucursales/composables/useSucursales';
 
 // dependencias
 const route = useRoute();
-const {obtenerUsuario, editarUsuario, reiniciarDataUsuarios, habilitarEdicion} = useUsuarios();
+const { obtenerUsuario, reiniciarDataUsuarios, habilitarEdicion, editarUsuario } = useUsuarios();
+const { obtenerAreas } = useAreas();
+const { obtenerClientes } = useClientes();
 const { obtenerPerfiles } = usePerfiles();
-const { obtenerAreas} = useAreas();
-const { obtenerClientes} = useClientes();
-const { obtenerSucursales} = useSucursales();
-
+const { obtenerSucursales } = useSucursales();
 
 // componentes
 const VDataView = defineAsyncComponent(() => import('@/views/detalles/VDataView.vue'));
-const UsuarioFormBase = defineAsyncComponent(() => import('../components/forms/UsuarioFormBase.vue'));
+const UsuariosFormularioBase = defineAsyncComponent(() => import('../components/forms/UsuariosFormularioBase.vue'));
 
+// configuracion vista
 const peticiones = ref([
-    obtenerUsuario({id: route.params.id}),
-    obtenerAreas({params:{ listado: true }}),
-    obtenerClientes({params:{ listado: true }}),
-    obtenerSucursales({params:{ listado: true }}),
-    obtenerPerfiles({params:{ listado: true }}),
+    obtenerUsuario({ id: route.params.id }),
+    obtenerAreas({ params: { listado: true } }),
+    obtenerClientes({ params: { listado: true } }),
+    obtenerPerfiles({ params: { listado: true } }),
+    obtenerSucursales({ params: { listado: true } }),
 ])
 </script>
