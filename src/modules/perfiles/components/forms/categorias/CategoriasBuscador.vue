66da8d39954aac34550df1a7b4298952f8593fb2
Luis Mario Torres Moreno <luisillo77mario@gmail.com>
2025-03-24 16:31:16 -0700

    feat(components/forms/categorias):creados los formularios para agregar y buscar categorias

---
+++ src/modules/perfiles/components/forms/categorias/CategoriasBuscador.vue
@@ -0,0 +1,25 @@
+<template>
+    <VBuscadorBase :="confFormulario">
+        <template #contenido>
+            <CategoriasBuscadorBase/>
+        </template>
+    </VBuscadorBase>
+</template>
+
+<script setup>
+import {ref, defineAsyncComponent} from 'vue';
+import useCategorias from '../../../composables/useCategorias';
+
+// dependencias
+const {obtenerCategorias} = useCategorias();
+
+// componentes
+const VBuscadorBase = defineAsyncComponent(() => import('@/components/forms/VBuscadorBase.vue'));
+const CategoriasBuscadorBase = defineAsyncComponent(() => import('./CategoriasBuscadorBase.vue'));
+
+// data
+const confFormulario = ref({
+    tituloBoton: 'buscar',
+    accionFormulario: obtenerCategorias,
+})
+</script>
---
+++ src/modules/perfiles/components/forms/categorias/CategoriasFormularioBase.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">nombre</p>
+            <NInput 
+                v-model:value="categoria.nombre"
+                clearable
+                :disabled="edicionHabilitada"/>
+        </article>
+    </section>
+</template>
+
+<script setup>
+import { NInput } from 'naive-ui'
+import useCategorias from '../../../composables/useCategorias';
+
+//dependencias
+const { categoria, edicionHabilitada } = useCategorias();
+</script>
