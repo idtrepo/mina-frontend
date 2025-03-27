b2f65ac74ee9fcb1ac929264c4b52053f581533a
Luis Mario Torres Moreno <luisillo77mario@gmail.com>
2025-03-27 14:47:19 -0700

    feat(perfiles/forms/operaciones): create form components for operation management

---
+++ src/modules/perfiles/components/forms/operaciones/OperacionesFormulario.vue
@@ -0,0 +1,25 @@
+<template>
+    <VFormularioBase :="confFormulario">
+        <template #contenido>
+            <OperacionesFormularioBase/>
+        </template>
+    </VFormularioBase>
+</template>
+
+<script setup>
+import {ref, defineAsyncComponent} from 'vue';
+import useOperaciones from '../../../composables/useOperaciones';
+
+// dependencias
+const {crearOperacion} = useOperaciones();
+
+// componentes
+const VFormularioBase = defineAsyncComponent(() => import('@/components/forms/VFormularioBase.vue'));
+const OperacionesFormularioBase = defineAsyncComponent(() => import('./OperacionesFormularioBase.vue'));
+
+// configuracion formulario
+const confFormulario = ref({
+    tituloBoton: 'crear',
+    accionFormulario: crearOperacion,
+})
+</script>
---
+++ src/modules/perfiles/components/forms/operaciones/OperacionesFormularioBase.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">nombre</p>
+            <NInput 
+                v-model:value="operacion.nombre"
+                clearable
+                :disabled="edicionHabilitada"/>
+        </article>
+    </section>
+</template>
+
+<script setup>
+import { NInput } from 'naive-ui'
+import useOperaciones from '../../../composables/useOperaciones';
+
+//dependencias
+const { operacion, edicionHabilitada } = useOperaciones();
+</script>
