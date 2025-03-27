9bdfb78a344a2d98488d4e8b3e49000278c059c4
Luis Mario Torres Moreno <luisillo77mario@gmail.com>
2025-03-27 14:42:45 -0700

    feat(perfiles/forms/acciones): add search and filter components for actions

---
+++ src/modules/perfiles/components/forms/acciones/AccionesBuscadorBase.vue
@@ -0,0 +1,50 @@
+<template>
+    <section>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">nombre</p>
+            <NInput v-model:value="filtros.nombre"/>
+        </article>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">categoria</p>
+            <NSelect 
+                v-model:value="filtros.categoria"
+                :options="categoriasOpciones"/>
+        </article>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">operacion</p>
+            <NSelect 
+                v-model:value="filtros.operacion"
+                :options="operacionesOpciones"/>
+        </article>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">fecha</p>
+            <NDatePicker
+                v-model:value="filtros.fecha"
+                clearable/>
+        </article>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">estatus</p>
+            <NSwitch
+                v-model:value="filtros.estatus"/>
+        </article>
+    </section>
+</template>
+
+<script setup>
+import { NDatePicker, NSwitch, NSelect, NInput } from 'naive-ui';
+import {onMounted} from 'vue';
+import useAcciones from '../../../composables/useAcciones';
+import useCategorias from '@/modules/perfiles/composables/useCategorias';
+import useOperaciones from '@/modules/perfiles/composables/useOperaciones';
+
+// dependencias
+const { filtros } = useAcciones();
+const { categoriasOpciones, obtenerCategorias } = useCategorias();
+const { operacionesOpciones, obtenerOperaciones } = useOperaciones();
+
+// ciclo de vida
+onMounted(() => {
+    obtenerCategorias({params:{listado:true}});
+    obtenerOperaciones({params:{listado:true}});
+})
+</script>
