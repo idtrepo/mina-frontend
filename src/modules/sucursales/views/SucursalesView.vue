fcbb0871b7b3af449a15eff75cb058ab2cee142f
luisillo77D <luisillo77mario@gmail.com>
2025-05-16 16:42:17 -0700

    feat(SucursalesView): refactorizado la estructura y agregado un modal extra en el listado junto con su boton

--- src/modules/sucursales/views/SucursalesView.vue
+++ src/modules/sucursales/views/SucursalesView.vue
@@ -1,15 +1,19 @@
 <template>
-    <VListadoView
-        :elementos="sucursalesListado"
-        :numElementos="numeroElementos"
-        :obtenerListado = "obtenerSucursales"
-        :reiniciarData = "reiniciarDataCreacion"
-        >
+    <VListadoView :elementos="sucursalesListado" :numElementos="numeroElementos" :obtenerListado="obtenerSucursales"
+        :reiniciarData="reiniciarDataCreacion">
         <template #formulario-buscar>
-            <SucursalesBuscador/>
+            <SucursalesBuscador />
         </template>
         <template #formulario-agregar>
-            <SucursalesFormulario/>
+            <SucursalesFormulario />
+        </template>
+        <template #formulario-extra>
+            <formularioReporte></formularioReporte>
+        </template>
+        <template #boton-extra="{ mostrarModalExtra }">
+            <NButton @click="mostrarModalExtra" type="info">
+                <span class="uppercase font-bold">reporte</span>
+            </NButton>
         </template>
     </VListadoView>
 </template>
@@ -19,6 +23,7 @@ import { onUnmounted, onMounted } from 'vue'
 import { defineAsyncComponent } from 'vue'
 import useSucursales from '@/modules/sucursales/composables/useSucursales';
 import useClientes from '@/modules/clientes/composables/useClientes';
+import { NButton } from 'naive-ui';
 
 // dependencias
 const { sucursalesListado, numeroElementos, obtenerSucursales, reiniciarDataSucursales, reiniciarDataCreacion } = useSucursales();
@@ -28,6 +33,7 @@ const { obtenerClientes } = useClientes();
 const VListadoView = defineAsyncComponent(() => import('@/views/listado/VListadoView.vue'));
 const SucursalesBuscador = defineAsyncComponent(() => import('../components/forms/SucursalesBuscador.vue'));
 const SucursalesFormulario = defineAsyncComponent(() => import('../components/forms/SucursalesFormulario.vue'));
+const formularioReporte = defineAsyncComponent(() => import('@/components/forms/reporteFormulario.vue'));
 
 // ciclo de vida
 onMounted(() => {
