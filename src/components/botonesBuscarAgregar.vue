b2de101b22b6c48f1442173087ca2709227a177b
luisillo77D <luisillo77mario@gmail.com>
2025-05-16 16:40:46 -0700

    feat(components/botonesBuscarAgregar): agregado componente con los modales default en los listado

---
+++ src/components/botonesBuscarAgregar.vue
@@ -0,0 +1,19 @@
+<template>
+    <NModal v-model:show="verModalAgregar">
+        <NCard class="w-3/4 md:w-1/3">
+            <slot name="formulario-agregar"></slot>
+        </NCard>
+    </NModal>
+    <NModal v-model:show="verModalBuscar">
+        <NCard class="w-3/4 md:w-1/3">
+            <slot name="formulario-buscar"></slot>
+        </NCard>
+    </NModal>
+</template>
+<script setup>
+import { NModal, NCard } from 'naive-ui'
+import {inject} from 'vue'
+
+const verModalAgregar = inject('verModalAgregar')
+const verModalBuscar = inject('verModalBuscar')
+</script>
