b53b038eb991415a720424912d466a58261524b7
luisillo77D <luisillo77mario@gmail.com>
2025-05-20 16:22:18 -0700

    feat(sensores/components/forms): agregados fomularios para la busqueda, creacion, y edicion de los sensores

---
+++ src/modules/modulos/components/forms/SensoresBuscadorBase.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">clave</p>
+            <NInput v-model:value="filtros.clave"/>
+        </article>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">identificador</p>
+            <NInput
+                v-model:value="filtros.identificador"
+                clearable/>
+        </article>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">fecha</p>
+            <NDatePicker
+                v-model:value="filtros.fecha"
+                clearable/>
+        </article>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">estatus</p>
+            <NSwitch
+                v-model:value="filtros.estatus"/>
+        </article>
+    </section>
+</template>
+
+<script setup>
+import { NDatePicker, NSwitch, NInput } from 'naive-ui'
+import useSensores from '../../composables/useSensores';
+
+//dependencias
+const { filtros } = useSensores();
+</script>
---
+++ src/modules/modulos/components/forms/SensoresFormularioBase.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">clave</p>
+            <NInput v-model:value="sensor.clave" :disabled="edicionHabilitada"/>
+        </article>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">umbral</p>
+            <NInput v-model:value="sensor.umbral" type="number" clearable @update:value="val => sensor.umbral = parseInt(val)" :disabled="edicionHabilitada"/>
+        </article>
+        <article class="mb-4">
+            <p class="mb-1 uppercase">identificador</p>
+            <NInput v-model:value="sensor.identificador" :disabled="edicionHabilitada"/>
+        </article>
+    </section>
+</template>
+<script setup>
+import { NInput } from 'naive-ui';
+import useSensores from '../../composables/useSensores';
+//dependencias
+const { sensor, edicionHabilitada } = useSensores();
+</script>
