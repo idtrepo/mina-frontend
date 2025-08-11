24751f8a7780b925f3d57556f231cc0574e04c37
= <luisillo77mario@gmail.com>
2025-08-11 10:17:50 -0700

    feat(notificaciones): agregado componente NotificacionesModoToggle para selección de modo de ingreso

---
+++ src/modules/modulos/components/forms/NotificacionesModoToggle.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex items-center justify-between">
+    <div class="text-sm font-medium">Modo de ingreso</div>
+    <NRadioGroup :value="modelValue" @update:value="$emit('update:modelValue', $event)" size="small">
+      <NRadioButton value="manual">Manual</NRadioButton>
+      <NRadioButton value="usuarios">Usuarios</NRadioButton>
+    </NRadioGroup>
+  </div>
+</template>
+
+<script setup>
+import { NRadioGroup, NRadioButton } from 'naive-ui';
+
+defineProps({
+  modelValue: { type: String, required: true },
+});
+
+defineEmits(['update:modelValue']);
+</script>
+
+<style scoped>
+</style>
