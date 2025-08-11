85fdcb28fffaaa2a60ee62eedb9ad086ab1ec2dd
= <luisillo77mario@gmail.com>
2025-08-11 10:16:13 -0700

    feat(modulos/views)agregado boton para mostrar modal de configuracion de notificaciones

--- src/modules/modulos/views/ModulosView.vue
+++ src/modules/modulos/views/ModulosView.vue
@@ -4,6 +4,14 @@
         :numElementos="numeroElementos"
         :obtenerListado="obtenerModulos"
         :reiniciarData="reiniciarDataCreacion">
+        <template v-if="usuarioPerfilId<4" #boton-extra="{ mostrarModalExtra }">
+            <NButton @click="mostrarModalExtra" type="info">
+                <span class="uppercase font-bold">Notificaciones</span>
+            </NButton>
+        </template>
+        <template #formulario-extra>
+           <FormularioNotificaciones />
+        </template>
         <template #formulario-buscar>
             <ModulosBuscador/>
         </template>
@@ -16,21 +24,25 @@
 <script setup>
 import { onMounted, onUnmounted } from 'vue'
 import { defineAsyncComponent } from 'vue'
+import { NButton } from 'naive-ui';
 import useModulos from '../composables/useModulos';
 import useAreas from '@/modules/areas/composables/useAreas';
 import useClientes from '@/modules/clientes/composables/useClientes';
 import useSucursales from '@/modules/sucursales/composables/useSucursales';
+import useUsuarioStore from '@/stores/useUsuarioStore';
 
 // dependencias
 const { modulosListado, numeroElementos, obtenerModulos, reiniciarDataModulos, reiniciarDataCreacion } = useModulos();
 const { obtenerAreas } = useAreas();
 const { obtenerClientes } = useClientes();
 const { obtenerSucursales } = useSucursales();
+const { usuarioPerfilId } = useUsuarioStore();
 
 // componentes
 const VListadoView = defineAsyncComponent(() => import('@/views/listado/VListadoView.vue'));
 const ModulosBuscador = defineAsyncComponent(() => import('../components/forms/ModulosBuscador.vue'));
 const ModulosFormulario = defineAsyncComponent(() => import('../components/forms/ModulosFormulario.vue'));
+const FormularioNotificaciones = defineAsyncComponent(() => import('../components/forms/NotificacionesFormulario.vue'));
 
 onMounted(() => {
     Promise.allSettled([
