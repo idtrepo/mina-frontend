1abc2fa3fbc5d86ac6aa1e80cbab99d4e604863a
= <luisillo77mario@gmail.com>
2025-08-13 09:03:43 -0700

    fix(botonesBuscarAgregar): fix del ancho de los modales

--- src/components/botonesBuscarAgregar.vue
+++ src/components/botonesBuscarAgregar.vue
@@ -1,18 +1,22 @@
 <template>
     <NModal v-model:show="verModalAgregar">
-        <NCard class="w-3/4 md:w-1/3">
-            <slot name="formulario-agregar"></slot>
-        </NCard>
+        <section class="md:w-1/2 w-3/4">
+            <NCard>
+                <slot name="formulario-agregar"></slot>
+            </NCard>
+        </section>
     </NModal>
     <NModal v-model:show="verModalBuscar">
-        <NCard class="w-3/4 md:w-1/3">
-            <slot name="formulario-buscar"></slot>
-        </NCard>
+        <section class="md:w-1/2 w-3/4">
+            <NCard>
+                <slot name="formulario-buscar"></slot>
+            </NCard>
+        </section>
     </NModal>
 </template>
 <script setup>
 import { NModal, NCard } from 'naive-ui'
-import {inject} from 'vue'
+import { inject } from 'vue'
 
 const verModalAgregar = inject('verModalAgregar')
 const verModalBuscar = inject('verModalBuscar')
